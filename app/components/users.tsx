4cf405c2bc51f3e543cd2faa8275e3d9217b8abb
Peter Nyiro <nyiropet@gmail.com>
2025-06-30 19:36:58 +0200

    setup basic config

---
+++ app/components/users.tsx
@@ -0,0 +1,7 @@
+export function Users() {
+  return (
+    <main>
+      <h1>User dashboard</h1>
+    </main>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Users } from "../components/users";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "User dashboard" },
+    { name: "description", content: "Welcome to User dashboard!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Users />;
 }
