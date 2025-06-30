3a20b10c9e0f2f0bc465016b59f1862a6ecbce15
Peter Nyiro <nyiropet@gmail.com>
2025-06-30 21:09:48 +0200

    renders users in table

--- app/components/users-table.tsx
+++ app/components/users-table.tsx
@@ -1,29 +1,63 @@
-type UsersTableProps = {
-  users: Array<{
-    id: number;
+import { Button } from "@mui/material";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+  type MRT_ColumnDef,
+} from "material-react-table";
+import { useMemo } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  company: {
     name: string;
-    username: string;
-    email: string;
-    company: {
-      name: string;
-      catchPhrase: string;
-      bs: string;
-    };
-  }>;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+type UsersTableProps = {
+  users: User[];
 };
 
 export function UsersTable({ users }: UsersTableProps) {
-  return (
-    <main>
-      <h1>User dashboard</h1>
+  const data = useMemo(() => users, [users]);
+  const columns = useMemo<MRT_ColumnDef<User>[]>(
+    () => [
+      {
+        accessorKey: "name",
+        header: "Name",
+        size: 150,
+      },
+      {
+        accessorKey: "username",
+        header: "Username",
+        size: 150,
+      },
+      {
+        accessorKey: "email",
+        header: "Email",
+        size: 200,
+      },
+      {
+        accessorKey: "company.name",
+        header: "Company Name",
+        size: 200,
+      },
+    ],
 
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name} ({user.username}) - {user.email}
-          </li>
-        ))}
-      </ul>
+    []
+  );
+  const table = useMaterialReactTable({
+    columns,
+    data,
+  });
+
+  return (
+    <main className="m-4">
+      <MaterialReactTable table={table} />
     </main>
   );
 }
